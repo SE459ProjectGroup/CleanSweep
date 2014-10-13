0d0f14b8cc28c8094d26b11972860bceea861d58
John Reagan <jreagan@bestcase.com>
2014-10-13 17:55:13 -0500

    Removed Mockito Dependency
    Removed Mockito Dependency for now until it is required. DIdnt want
    this to prevent anyone from building.

--- src/Navigation/NavigationTests.java
+++ src/Navigation/NavigationTests.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import static org.mockito.Mockito.*;
+//import static org.mockito.Mockito.*;
 
 public class NavigationTests {
 
