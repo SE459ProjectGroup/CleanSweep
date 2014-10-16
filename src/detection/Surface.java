67b85688ac13c477f44d03484e0aa7a7b6a3b81b
Bruna Moreira <brunanayaramlima@gmail.com>
2014-10-16 18:58:24 -0500

    Create Detection package and Add Method Get Surface

---
+++ src/detection/Surface.java
@@ -0,0 +1,32 @@
+package detection;
+
+import Navigation.Coordinate;
+import parser.ParseXML;
+
+public class Surface {
+	
+	int surf_type;
+	String[] surface_types = { null, "bare_floor", "low_carpet", "high_carpet" };
+	//static ParseXML parse;
+
+	public int getSurf_type() {
+		return surf_type;
+	}
+
+	public void setSurf_type(int surf_type) {
+		this.surf_type = surf_type;
+	}
+	
+	public Surface(int surface){
+		this.surf_type = surface;
+	}
+	
+	public String getSurfaceFromCoordinate(Coordinate coord){
+		String surface = null;
+		
+		surf_type = ParseXML.getSurfaceTypeFromCoordinates(coord.getX(), coord.getY());
+		surface = surface_types[surf_type];
+		
+		return surface;
+	}
+}
